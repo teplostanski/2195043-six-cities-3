9b139f936329b3e530f2ae6e058810b431c041c3
teplostanski <igor@teplostanski.me>
2026-04-27 17:16:29 +0300

    Сужает типизицию функции-селектора

--- src/store/selectors/offerSelectors.ts
+++ src/store/selectors/offerSelectors.ts
@@ -1,4 +1,4 @@
-import type { OfferPreview } from '../../shared/types';
+import type { CityName, OfferPreview } from '../../shared/types';
 
-export const selectOffersByCity = (offers: OfferPreview[], city: string) =>
+export const selectOffersByCity = (offers: OfferPreview[], city: CityName) =>
   offers.filter((offer) => offer.city.name === city);
